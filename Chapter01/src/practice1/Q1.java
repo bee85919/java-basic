2e3aabd87aabf9bc643e6b6a9e1fe1c4759629cb
bee <bee85919@gmail.com>
2023-07-17 23:18:37 +0900

    study practice Q1~Q4

---
+++ Chapter01/src/practice1/Q1.java
@@ -0,0 +1,34 @@
+package practice1;
+
+import java.util.Scanner;
+
+public class Q1 {
+
+	public static void main(String[] args) {
+
+//		grade = (score >= 90) ? 'A':'B';
+		
+		char grade;
+		int score;
+		
+		Scanner scanner = new Scanner(System.in);		
+		
+		System.out.println("Enter grade: ");
+		score = scanner.nextInt();
+		
+		if (score >= 90) {
+			
+			grade = 'A';
+			
+		}
+		else {
+			
+			grade = 'B';
+			
+		}
+		
+		System.out.println("Grade: " + grade);
+		
+	}
+
+}
